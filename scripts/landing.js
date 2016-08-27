2526dbb765786668e4fa8502eb5c297ffcb0173c
apennick <anne.pennick@gmail.com>
2016-08-27 14:44:23 -0500

    checkpoint 9 updated landing.js animations for window height

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
   var revealPoint = function(index) {
     points[index].style.opacity = 1;
@@ -9,7 +9,21 @@ var animatePoints = function() {
     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
   }
 
-  for (var i=0, 1 < points.length; i++) {
+  for (var i=0; 1 < points.length; i++) {
     revealPoint(i);
   }
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  
+  var sellingPoints = document.getElementsByClassName ('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener("scroll", function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
