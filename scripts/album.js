4923ddad1f4f3852cee0b2b5c11ec23537c7ec70
apennick <anne.pennick@gmail.com>
2016-11-19 17:58:37 -0600

    Assignment 13 completed. Fixed findParentByClassName function.

--- scripts/album.js
+++ scripts/album.js
@@ -63,13 +63,20 @@ var setCurrentAlbum = function (album) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+    if (element.parentElement !== null) {
         var currentParent = element.parentElement;
         while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
+      
+    } else  if (element.parentElement === null){
+      alert("No parent found.");
+      
+    } else if (element.parentElement.className !== targetClass){
+      alert("No parent found with that class name.");
     }
+  
 };
 
 var getSongItem = function(element) {
