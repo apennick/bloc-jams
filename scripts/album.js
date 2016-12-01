92cde94e1675a733128b671f42a814b2e6654ee9
apennick <anne.pennick@gmail.com>
2016-11-30 19:17:11 -0600

    Maybe fixed something from checkpoint 17

--- scripts/album.js
+++ scripts/album.js
@@ -32,7 +32,7 @@ var albumMarconi = {
 
 var createSongRow = function (songNumber, songName, songLength) {
   var template =
-      '<tr class="album-view-song-item">'
+    '<tr class="album-view-song-item">'
   + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
   + ' <td class="song-item-title">' + songName + '</td>'
   + ' <td class="song-item-duration">' + songLength + '</td>'
