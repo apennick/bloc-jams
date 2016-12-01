acb52d0168fef698a7bfb406d07b022c152b2af5
apennick <anne.pennick@gmail.com>
2016-11-30 19:04:49 -0600

    Completed Checkpoint 17 by refactoring collection and album views with jQuery.

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function (){
+  var template =
   '<div class="collection-album-container column fourth">'
   +' <img src="assets/images/album_covers/01.png"/>'
   +' <div class="collection-album-info caption">'
@@ -12,16 +13,15 @@ var collectionItemTemplate =
   +'   </p>'
   +' </div>'
   +'</div>'
-;
-
-window.onload = function() {
-  // #1
-  var collectionContainer = document.getElementsByClassName('album-covers')[0];
-  // #2
-  collectionContainer.innerHTML = '';
   
-  // #3
+  return $(template);
+};
+
+$(window).load(function){
+  var $collectionContainer = $('.album-covers');
+  $collectionContainer.empty();
   for (var i=0; i < 12; i++) {
-    collectionContainer.innerHTML += collectionItemTemplate;
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);
   }
-}
+});
